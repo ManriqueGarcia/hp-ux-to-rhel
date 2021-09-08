e4dd0b32d0403513b6c068a13e54f80795e86500
Manrique García <mangarci@redhat.com>
2021-09-08 11:15:54 +0200

    port for sitox

--- hpux2rhel.sh
+++ hpux2rhel.sh
@@ -25,7 +25,7 @@ function check_workdir () {
 
 function migrate_user() {
     echo "Migrating users"
-    scp ${1}:/etc/passwd ${workdir}/hpux-passwd
+    scp -P 33722 ${1}:/etc/passwd ${workdir}/hpux-passwd
 
 }
 
